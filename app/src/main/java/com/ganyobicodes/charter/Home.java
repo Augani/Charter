5469994bf0fbcc2e497ade7af28149fae4849913
Augustus Otu <Augani7@gmail.com>
2018-10-15 11:47:07 +0100

    Home page builds

--- app/src/main/java/com/ganyobicodes/charter/Home.java
+++ app/src/main/java/com/ganyobicodes/charter/Home.java
@@ -1,13 +1,26 @@
 package com.ganyobicodes.charter;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.hbb20.CountryCodePicker;
 
 public class Home extends AppCompatActivity {
+    TextInputEditText phoneNumber;
+    CountryCodePicker countryCodePicker;
+    Button sendcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        sendcode = findViewById(R.id.sendCode);
+        countryCodePicker = findViewById(R.id.ccp);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        countryCodePicker.registerCarrierNumberEditText(phoneNumber);
+
+
     }
 }
